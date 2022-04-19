30b4286d0fad2fefb14fba3dd5d179a60229f4ae
Mr-Coxall <Mr-Coxall@users.noreply.github.com>
2022-04-19 18:33:09 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -26,15 +26,14 @@ getImage().then(function (jsonData) {
   document.getElementById("api-image").innerHTML =
     '<img src="' + jsonData.url + '" alt="API image" width="500">'
   if (jsonData.artist_url != "none") {
-  document.getElementById("image-artist").innerHTML =
-    "<p>Artist: " +
-    '<a href="' +
-    jsonData.artist_url +
-    '">' +
-    jsonData.artist +
-    "</a>"    
+    document.getElementById("image-artist").innerHTML =
+      "<p>Artist: " +
+      '<a href="' +
+      jsonData.artist_url +
+      '">' +
+      jsonData.artist +
+      "</a>"
   } else {
     document.getElementById("image-artist").innerHTML = "<p>Artist: unknown</p>"
   }
-
 })
