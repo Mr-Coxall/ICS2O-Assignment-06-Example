b36b476dfb14f82301628b3beb548cda046c75c9
Patrick Coxall <mr.coxall@mths.ca>
2022-05-17 13:26:12 +0000

    changed fetching code

--- js/script.js
+++ js/script.js
@@ -13,20 +13,21 @@ if (navigator.serviceWorker) {
   })
 }
 
+
 /**
  * Get API info.
- */
-async function getImage() {
-  let response = await fetch("https://api.catboys.com/img")
-  let jsonData = await response.json()
+*/
+// code from: https://www.youtube.com/watch?v=670f71LTWpM
 
-  return jsonData
-}
-getImage().then(function (jsonData) {
-  document.getElementById("api-image").innerHTML =
+const getImage = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData)
+    document.getElementById("api-image").innerHTML =
     '<img src="' + jsonData.url + '" alt="API image" class="center">'
-  if (jsonData.artist_url != "none") {
-    document.getElementById("image-artist").innerHTML =
+    if (jsonData.artist_url != "none") {
+      document.getElementById("image-artist").innerHTML =
       "<p>Artist: " +
       '<a href="' +
       jsonData.artist_url +
@@ -36,4 +37,9 @@ getImage().then(function (jsonData) {
   } else {
     document.getElementById("image-artist").innerHTML = "<p>Artist: unknown</p>"
   }
-})
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+getImage("https://api.catboys.com/img")
