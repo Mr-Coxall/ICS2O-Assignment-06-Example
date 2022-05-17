0bf4e9b39cd5f7f28455b06892f62ee2e703cdaf
Mr-Coxall <Mr-Coxall@users.noreply.github.com>
2022-05-17 13:27:59 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -13,10 +13,9 @@ if (navigator.serviceWorker) {
   })
 }
 
-
 /**
  * Get API info.
-*/
+ */
 // code from: https://www.youtube.com/watch?v=670f71LTWpM
 
 const getImage = async (URLAddress) => {
@@ -25,18 +24,19 @@ const getImage = async (URLAddress) => {
     const jsonData = await result.json()
     console.log(jsonData)
     document.getElementById("api-image").innerHTML =
-    '<img src="' + jsonData.url + '" alt="API image" class="center">'
+      '<img src="' + jsonData.url + '" alt="API image" class="center">'
     if (jsonData.artist_url != "none") {
       document.getElementById("image-artist").innerHTML =
-      "<p>Artist: " +
-      '<a href="' +
-      jsonData.artist_url +
-      '">' +
-      jsonData.artist +
-      "</a>"
-  } else {
-    document.getElementById("image-artist").innerHTML = "<p>Artist: unknown</p>"
-  }
+        "<p>Artist: " +
+        '<a href="' +
+        jsonData.artist_url +
+        '">' +
+        jsonData.artist +
+        "</a>"
+    } else {
+      document.getElementById("image-artist").innerHTML =
+        "<p>Artist: unknown</p>"
+    }
   } catch (err) {
     console.log(err)
   }
